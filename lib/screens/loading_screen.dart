766d9857b49817337839083d77c554bbdc1fae0e
lucashsmoraes <lucashsmoraes@gmail.com>
2020-02-25 16:40:08 -0300

    criando classe netWork e separando responsabilidades

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,16 +1,43 @@
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+
+
+const apiKey = '83bdd4f52dc90808bd5078d7e6e3b268';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
+  double latitude;
+  double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.latitude);
+    latitude = location.latitude;
+    longitude = location.latitude;
+
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/'
+        'weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var decodedData = await networkHelper.getData();
+
+    double temperature = decodedData['main']['temp'];
+    int condition = decodedData['weather'][0]['id'];
+    String cityName = decodedData['name'];
+
+    print(temperature);
+    print(condition);
+    print(cityName);
   }
 
   @override
@@ -19,7 +46,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            getLocation();
+            getLocationData();
           },
           child: Text('Get Location'),
         ),
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,9 +1,10 @@
-
 import 'package:geolocator/geolocator.dart';
 
 class Location {
   double latitude;
   double longitude;
+
+
 
   Future<void> getCurrentLocation() async {
     try {
